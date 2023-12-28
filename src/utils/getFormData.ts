78d2797ca6e1b2717afccf43ea661eacba0b4371
Isaac Rojas <isaac.rojas@usach.cl>
2023-12-28 18:14:46 -0300

    fix: no cache en fetch

--- src/utils/getFormData.ts
+++ src/utils/getFormData.ts
@@ -17,7 +17,9 @@ export type ApiData = {
 export type ApiBody = Array<ApiData>;
 
 export default async function getFormData(): Promise<ApiBody> {
-  const res = await fetch('https://run.mocky.io/v3/c7a96306-c122-4037-8b27-a2120b9e6f04');
+  const res = await fetch('https://run.mocky.io/v3/c7a96306-c122-4037-8b27-a2120b9e6f04', {
+    cache: 'no-cache'
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
